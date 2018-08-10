ec0b980444c1114ee5e7be8019f0eed7c2d98082
jersearls <searls@me.com>
2018-08-10 14:21:03 -0400

    blink and move

--- src/moneyCat.ino
+++ src/moneyCat.ino
@@ -1,24 +1,62 @@
-Servo myservo;  // create servo object to control a servo
-                // a maximum of eight servo objects can be created
+Servo catArm;  // create servo object to control a servo
 
-int pos = 0;    // variable to store the servo position
+#define LED 1
 
 void setup()
 {
-  myservo.attach(D0);  // attaches the servo on pin 0 to the servo object
-}
+  catArm.attach(D0);  // attaches the servo on pin 0 to the servo object
+  Particle.function("Wave", Wave) ;
 
+  pinMode(LED, OUTPUT);
+  Particle.function("ToggleLED", ToggleLED) ;
+
+  Particle.function("SmileWave", SmileWave) ;
+}
 
 void loop()
 {
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
+}
+
+int SmileWave(String message) {
+  digitalWrite(LED, HIGH) ;
+  Wave(message) ;
+  digitalWrite(LED, LOW) ;
+}
+
+// LED FUNCTIONS
+int ToggleLED(String message) {
+  if (message == "0") {
+    digitalWrite(LED, LOW);
+  }
+  else if (message == "1") {
+    digitalWrite(LED, HIGH);
+  }
+}
+
+//SERVO FUNCTIONS
+int Wave(String message) {
+  int i;
+  int numberOfWaves = message.toInt();
+  for (i = 0; i < numberOfWaves; i++) {
+    extendArm();
+    retractArm();
+  }
+}
+
+void extendArm() {
+  int pos = 0;
+  for(pos = 0; pos < 180; pos += 5)  // goes from 0 degrees to 180 degrees
   {                                  // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    catArm.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(25);                       // waits 15ms for the servo to reach the position
   }
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees
+}
+
+void retractArm() {
+  int pos = 0;
+  for(pos = 180; pos>=1; pos-= 5)     // goes from 180 degrees to 0 degrees
   {
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    catArm.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(25);                       // waits 15ms for the servo to reach the position
   }
-} 
+}
